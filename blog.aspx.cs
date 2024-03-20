86d60f43a677b8466ca51d208d7d9ab187695614
Yuvarani pandi.B <110343669+Yuvaranipandi@users.noreply.github.com>
2024-03-20 18:06:55 +0530

    Add files via upload

---
+++ blog.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication_master_testing
+{
+    public partial class WebForm7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
